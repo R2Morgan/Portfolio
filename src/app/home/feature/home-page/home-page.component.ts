8cc7eece30df19bb6198fb15f3fa293a059922ff
stef12121999 <stefandobrescu2008@yahoo.com>
2022-10-22 23:25:48 +0300

    22.10.2022 Commit

--- src/app/home/feature/home-page/home-page.component.ts
+++ src/app/home/feature/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
--- src/app/home/feature/home-page/home-page.module.ts
+++ src/app/home/feature/home-page/home-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
+import { HomepageScrollspyComponent } from '../../ui/homepage-scrollspy/homepage-scrollspy.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    HomepageScrollspyComponent
   ],
   imports: [
     CommonModule
